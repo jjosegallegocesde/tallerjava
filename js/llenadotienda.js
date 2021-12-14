9b1818c90799695fd2425a153969ba14cfa7e6e5
Carlos Ospina <69280489+dany-uz@users.noreply.github.com>
2021-12-14 17:10:26 -0500

    se crea index

---
+++ js/llenadotienda.js
@@ -0,0 +1,39 @@
+//Resolver probelas con JS utilizamos VARIABLES
+//VARIABLE es una "caja", un espacio en memoria para almacenar DATOS
+
+//JS ES UN LENGUAJE INTERPRETADO
+//JS ES UN LENGUAJE DINAMICAMENTE TIPADO
+
+//ARREGLO es una variable especial que me permite almacenar varios datos en
+//1 SOLA VARIABLE
+/*let productos=["camiseta","gorra","termo"]
+console.log(productos)
+console.log(productos[0])*/
+
+//OBJETO es una variable especial que almacena muchos datos DIFERRENTES
+//en una sola variable
+/*let objetoProductos={
+    nombre:"Camiseta",
+    precio:50000,
+    cantidad:5,
+    foto:"http://www.fotos.com"
+}
+console.log(objetoProductos)
+console.log(objetoProductos.nombre)*/
+
+//MAIZENA DE ARREGLOS CONS ABOR A OBJETOS
+let productos=[
+    {
+        nombre:"camiseta",
+        precio:50000,
+        cantidad:0,
+        foto:""
+    },
+    {
+        
+    },
+    {},
+    {}
+]
+
+
